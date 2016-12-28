4954f680be2431b9efafc039321181c3459f52e0
dan plummer <danplumme6@fullsail.edu>
2016-12-28 17:30:38 -0600

    Organized admin page html and nav

--- includes/navigation.php
+++ includes/navigation.php
@@ -28,10 +28,11 @@
                     ?>        
             
 
-                    <!--
+                    
                     <li>
-                        <a href="#">About</a>
+                        <a href="admin">Admin</a>
                     </li>
+                    <!--
                     <li>
                         <a href="#">Services</a>
                     </li>
